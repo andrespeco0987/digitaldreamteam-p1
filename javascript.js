645c6eaa1686d0a7c3002b2da016321a122e15dc
Andres Felipe Rangel Peña <andrespeco0987@gmail.com>
2024-04-14 12:23:09 -0500

    formulario completo e inicio de apartado nosotros

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+/* animaciones html imagenes */
 window.addEventListener('scroll', () => {
     const images = document.querySelectorAll('.container-sm');
     const paragraphs = document.querySelectorAll('.container-sm p');
@@ -24,3 +25,35 @@ function isElementInViewport(el) {
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
+
+/* Codigo formulario */
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+
+    const $nombre = document.getElementById('nombre').value;
+    const $apellidos = document.getElementById('apellidos').value;
+    const $correo = document.getElementById('correo').value;
+    const $codigoPais = document.getElementById('codigoPais').value;
+    const $celular = document.getElementById('celular').value;
+    const $mensaje = document.getElementById('mensaje').value;
+
+  
+    console.log('Nombre:', $nombre);
+    console.log('Apellidos:', $apellidos);
+    console.log('Correo electrónico:', $correo);
+    console.log('Código de País:', $codigoPais);
+    console.log('Número de Celular:', $celular);
+    console.log('Mensaje:', $mensaje);
+
+  
+    alert('Su mensaje fue enviado exitosamente.');
+
+
+    document.getElementById('nombre').value = '';
+    document.getElementById('apellidos').value = '';
+    document.getElementById('correo').value = '';
+    document.getElementById('codigoPais').value = 'null'; 
+    document.getElementById('celular').value = '';
+    document.getElementById('mensaje').value = '';
+});
